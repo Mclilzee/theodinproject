e320d2919d57cdb482e53ecd72394a5e2af604d7
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-07-17 06:40:44 +0200

    Update show response to include user ranking

--- app/controllers/api/points_controller.rb
+++ app/controllers/api/points_controller.rb
@@ -9,9 +9,14 @@ class Api::PointsController < ApplicationController
   def show
     sql_query = 'SELECT *, rank FROM (SELECT *, RANK() OVER (ORDER BY points DESC, id) FROM points) AS rank WHERE discord_id = ? LIMIT 1;'
     user = Point.find_by_sql([sql_query, params[:id]])
+||||||| parent of 1ed6be59 (Update show response to include user ranking)
+    user_points = Point.find_by(discord_id: params[:id])
 
     if user.present?
       render json: user.first
+||||||| parent of 1ed6be59 (Update show response to include user ranking)
+    if user_points.present?
+      render json: user_points
     else
       render json: { message: 'Unable to find that user' }
     end
