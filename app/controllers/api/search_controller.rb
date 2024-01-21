e9d8aae138fdecc7007f305de4d29b8993f9e113
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-21 01:46:53 +0100

    Update endpoint

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -3,29 +3,37 @@ class Api::SearchController < ApplicationController
   before_action :authenticate
 
   def index
-    query = params[:query].scan(/\b\w+\b/)
-    lesson_ids = WordFrequency
-                 .where(word: query)
-                 .group(:lesson_id)
-                 .select('lesson_id, SUM(tf_idf) as total_tf_idf')
-                 .order('total_tf_idf DESC')
-                 .map(&:lesson_id)
-
-    uuid = Set.new
-    lessons_hash = {}
-    lessons = Lesson
-              .where(id: lesson_ids)
-              .each do |lesson|
-      unless uuid.include?(lesson.title)
-        lessons_hash[lesson.id] = { url: 'https://www.theodinproject.com/lessons/' + lesson.slug, title: lesson.title, desc: lesson.description }
-        uuid.add(lesson.title)
-      end
-    end
-
-    render json: lesson_ids.map { |id| lessons_hash[id] }.compact
+    query = params[:q].scan(/\b\w+\b/)
+    path = find_path(query)
+    records = TfIdf
+              .where(word: query)
+              .group(:search_record_id)
+              .select('search_record_id, SUM(score) as total_score')
+              .order('total_score DESC')
+              .map(&:search_record_id)
+              .flat_map { |id| path ? SearchRecord.where(id:, path:) : SearchRecord.where(id:) }
+    render json: records
   end
 
   private
+
+  def find_path(query)
+    if query.intersect?(%w[javascript node nodejs js])
+      'full-stack-javascript'
+    elsif query.intersect?(%w[ruby rails rail])
+      'full-stack-ruby-on-rails'
+    elsif query.intersect?(%w[foundation foundations])
+      'foundations'
+    else
+      nill
+    end
+  end
+
+  def retrieve_records(ids)
+    ids.map do |id|
+      SearchRecord.where(id:)
+    end
+  end
 
   def authenticate
     authenticate_or_request_with_http_token do |token|
